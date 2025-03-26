7c7dae6227528a96ee2dd7f20bc6b6d16b26b6ba
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2025-03-26 01:22:32 -0300

    Add exception handler for an unexpected error

--- src/main/java/com/drivelab/autocenter/rest/GlobalExceptionHandler.java
+++ src/main/java/com/drivelab/autocenter/rest/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.drivelab.autocenter.rest;
 
 import com.drivelab.autocenter.domain.DomainException;
 import com.drivelab.autocenter.domain.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +12,8 @@ import org.springframework.web.reactive.result.method.annotation.ResponseEntityE
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<ProblemDetails> domainResponse(DomainException ex) {
@@ -21,5 +25,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ProblemDetails> entityNotFoundResponse(EntityNotFoundException ex) {
         ProblemDetails problemDetails = new ProblemDetails(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetails);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetails> exceptionResponse(Exception ex) {
+        ProblemDetails problemDetails = new ProblemDetails("An unexpected error occurred. Contact the system administrator");
+        logger.error(ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problemDetails);
     }
 }
